8733777c86ad18c84f7ba8130ec013141b3ddcaa
Katayoon Alamy <katayoon.8379@gmail.com>
2024-07-22 21:37:12 +0330

    form with no style

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Form() {
-  return <div>form</div>;
+  return (
+    <div>
+      <h1>وب اپلیکیشن مدیریت مخاطبین</h1>
+      <input type="text" placeholder="نام..." />
+      <input type="text" placeholder="نام خانوادگی..." />
+      <input type="number" placeholder="شماره تماس..." />
+      <select name="نسبت" id="">
+        <option value="اعضای خانواده"></option>
+        <option value="دوست"></option>
+        <option value="همکار"></option>
+        <option value="فامیل"></option>
+      </select>
+      <input type="email" placeholder="ایمیل..." />
+    </div>
+  );
 }
